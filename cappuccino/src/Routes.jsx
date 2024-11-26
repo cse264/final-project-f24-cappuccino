8d604acc451c082f9cb3884f2bb230f1eaee2bfc
Alex Burmeister <aeb225@lehigh.edu>
2024-11-25 19:23:40 -0500

    simplified profile page

--- cappuccino/src/Routes.jsx
+++ cappuccino/src/Routes.jsx
@@ -6,6 +6,7 @@ import AddIdea from "./pages/AddIdea";
 import Weather from "./pages/Weather";
 import YourIdeas from "./pages/YourIdeas";
 import AllIdeas from "./pages/AllIdeas";
+import Profile from "./pages/Profile";
 // Import other components as needed
 
 const AppRoutes = () => {
@@ -14,12 +15,13 @@ const AppRoutes = () => {
       <Routes>
         {/* Redirect root to Login */}
         <Route path="/" element={<Login />} />
-        {/* Define all your routes */}
-        <Route path="/allideas" element={<AllIdeas />} />
         <Route path="/home" element={<Home />} />
-        <Route path="/addidea" element={<AddIdea />} />
+        {/* Define all your routes */}
         <Route path="/weather" element={<Weather />} />
         <Route path="/yourideas" element={<YourIdeas />} />
+        <Route path="/allideas" element={<AllIdeas />} />
+        <Route path="/addidea" element={<AddIdea />} />
+        <Route path="/profile" element={<Profile />} />
         {/* Add a catch-all for undefined routes */}
         <Route path="*" element={<h1>Page Not Found</h1>} />
       </Routes>

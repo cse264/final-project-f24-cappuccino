9bd8d14324e816423d6e1109b6464a8186499357
Alex Burmeister <aeb225@lehigh.edu>
2024-12-01 17:11:45 -0500

    remove home page

--- cappuccino/src/Routes.jsx
+++ cappuccino/src/Routes.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/LogIn/index.jsx"; // Adjust the path based on your file structure
-import Home from "./pages/Home";
 import AddIdea from "./pages/AddIdea";
 import Weather from "./pages/Weather";
 import YourIdeas from "./pages/YourIdeas";
@@ -15,7 +14,7 @@ const AppRoutes = () => {
       <Routes>
         {/* Redirect root to Login */}
         <Route path="/" element={<Login />} />
-        <Route path="/home" element={<Home />} />
+
         {/* Define all your routes */}
         <Route path="/weather" element={<Weather />} />
         <Route path="/yourideas" element={<YourIdeas />} />

aad1d330029b3b10e0453822648470e70b990dab
Alex Burmeister <aeb225@lehigh.edu>
2024-11-30 20:35:00 -0500

    changed port 5001

--- cappuccino/backend/server.js
+++ cappuccino/backend/server.js
@@ -42,7 +42,7 @@ app.get('/', (req, res) => {
 });
 
 // Start Server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 }).on('error', (err) => {
